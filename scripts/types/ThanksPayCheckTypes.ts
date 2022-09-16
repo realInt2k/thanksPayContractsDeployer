173a29c9ab6668f50e4d3fd94f4091394cf46a01
int2k <dungduychinguyen@gmail.com>
2022-09-16 21:04:32 +0900

    new typing

--- scripts/types/ThanksPayCheckTypes.ts
+++ scripts/types/ThanksPayCheckTypes.ts
@@ -17,10 +17,53 @@ type WorkerGetsThanksPayCheckType = {
     wId: number,
     pId: number,
 }
+
+type RegisterWorkerCheckType = {
+    wId: number,
+    pId: number,
+    wage: number
+}
+
+type SetWorkerPartnerCheckType = {
+    wId: number,
+    pId: number,
+}
+
+type RegisterPartnerCheckType = {
+    pId: number,
+    latestPay: number,
+}
+
+type SetLatestWagePayCheckType = {
+    pId: number,
+    timestamp: number,
+}
+
+type SetLatestRequestCheckType = {
+    wId: number,
+    latestRequest: number
+}
+
+type SetWorkerBalanceCheckType = {
+    wId: number,
+    newBalance: number
+}
+
+type SetPartnerBalanceCheckType = {
+    pId: number,
+    newBalance: number
+}
   
 export type ThanksPayCheckType = {
     workerGetSalaryEarlyCheck: WorkerGetsSalaryEarlyCheckType
     subtractFromPartnerCheck: SubtractFromPartnerCheckType,
     partnerWithdrawCheck: PartnerWithdrawCheckType,
-    workerGetsThanksPayCheck: WorkerGetsThanksPayCheckType
+    workerGetsThanksPayCheck: WorkerGetsThanksPayCheckType,
+    registerWorkerCheck: RegisterWorkerCheckType,
+    setWorkerPartnerCheck: SetWorkerPartnerCheckType,
+    registerPartnerCheck: RegisterPartnerCheckType,
+    setLatestWagePayCheck: SetLatestWagePayCheckType,
+    setLatestRequestCheck: SetLatestRequestCheckType,
+    setWorkerBalanceCheck: SetWorkerBalanceCheckType,
+    setPartnerBalanceCheck: SetPartnerBalanceCheckType
 }
